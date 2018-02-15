0fe06a31708e1f7b9180ddc3f7bb6607c7009024
Dmitri <unabl4@gmail.com>
2018-02-15 20:28:16 +0200

    Python CodeClub 'Hello World' challenge solution

---
+++ hello/hello.py
@@ -0,0 +1,7 @@
+# https://codeclub.thorgate.eu/challenges/1
+def hello(name):
+    return 'Hello ' + name
+
+# assertions (tests)
+assert hello('Taavi') == 'Hello Taavi'
+assert hello('Dmitri') == 'Hello Dmitri'
