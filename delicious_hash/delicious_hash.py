154a15b3a73fc6370aa86078356a5d5bfb1fd3f1
Dmitri <unabl4@gmail.com>
2018-03-14 23:27:42 +0200

    split the 'Delicious Hash' solution into two: the golf one (i.e shortest) and a normal, human-readable one

--- delicious_hash/delicious_hash.py
+++ delicious_hash/delicious_hash.py
@@ -1,7 +1,10 @@
+# https://codeclub.thorgate.eu/challenges/33
 import math,hashlib
-def crack_hash(t,c):
- for i in range(10):
-  v=(t+str(i))*math.ceil(20/(len(t)+1))
-  for y in [v,v.title()]:
-   if hashlib.md5(y.encode()).hexdigest() == c:
-    return y
+
+def crack_hash(thing, correct_hash):
+    times = math.ceil(20/(len(t)+1))
+    for i in range(10):
+        v=(thing+str(i)) * times
+        for y in [v,v.title()]:
+            if hashlib.md5(y.encode()).hexdigest() == correct_hash:
+                return y
