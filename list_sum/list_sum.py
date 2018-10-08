c14a38672ff5d3e69a54f7f1dd0a5d8741930474
Dmitri <unabl4@gmail.com>
2018-10-08 23:11:30 +0300

    'List sum' challenge solution

---
+++ list_sum/list_sum.py
@@ -0,0 +1,2 @@
+def list_sum(list_of_numbers):
+    return sum(x*x for x in list_of_numbers)
---
+++ list_sum/list_sum_test.py
@@ -0,0 +1,4 @@
+from list_sum import list_sum
+
+assert list_sum([1,1,1]) == 3
+assert list_sum([2,3,4]) == 29  # 4+9+16
