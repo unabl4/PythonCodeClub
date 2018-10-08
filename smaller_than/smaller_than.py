f79ffd5f2bbcd66e5427232ec415070cbb023437
Dmitri <unabl4@gmail.com>
2018-10-08 23:33:49 +0300

    'Smaller than' challenge solution

---
+++ smaller_than/smaller_than.py
@@ -0,0 +1,2 @@
+def smaller_than(number_1, number_2):
+    return None if number_1 == number_2 else min(number_1, number_2)
---
+++ smaller_than/smaller_than_test.py
@@ -0,0 +1,5 @@
+from smaller_than import smaller_than
+
+assert smaller_than(1,1) == None
+assert smaller_than(1,2) == 1
+assert smaller_than(2,1) == 1
